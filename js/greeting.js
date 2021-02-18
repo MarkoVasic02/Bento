aaa5bcd5afed06a0da026c0712bcc0aba233cef2
Marko <vasicmarko390@gmail.com>
2021-02-18 12:06:49 +0100

    changed greetings

--- js/greeting.js
+++ js/greeting.js
@@ -6,8 +6,8 @@ const hour = today.getHours();
 const name = ' Marko';
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep!  ';
-const gree2 = 'Good morning!  ';
+const gree1 = 'Go to Sleep  ' + name + '!';
+const gree2 = 'Good morning  ' + name + '!';
 const gree3 = 'Good afternoon  ';
 const gree4 = 'Good evening,  ';
 const gree5 = 'Good evening,  ';
@@ -15,9 +15,9 @@ const gree6 = 'Good evening,  ';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
-  document.getElementById('greetings').innerText = gree1 + name;
+  document.getElementById('greetings').innerText = gree1;
 } else if (hour >= 6 && hour < 12) {
-  document.getElementById('greetings').innerText = gree2 + name;
+  document.getElementById('greetings').innerText = gree2;
 } else if (hour >= 12 && hour < 17) {
   document.getElementById('greetings').innerText = gree3 + name;
 } else {
