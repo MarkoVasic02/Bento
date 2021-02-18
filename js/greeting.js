adcf6baa47dffb8a31e61c182ddd261ec417c456
Marko <vasicmarko390@gmail.com>
2021-02-18 12:09:31 +0100

    changed some greetings

--- js/greeting.js
+++ js/greeting.js
@@ -18,8 +18,8 @@ if (hour >= 23 && hour < 5) {
   document.getElementById('greetings').innerText = gree1;
 } else if (hour >= 6 && hour < 12) {
   document.getElementById('greetings').innerText = gree2;
-} else if (hour >= 12 && hour < 17) {
-  document.getElementById('greetings').innerText = gree3 + name;
+} else if (hour >= 12 && hour < 18) {
+  document.getElementById('greetings').innerText = gree3 + name + '.';
 } else {
-  document.getElementById('greetings').innerText = gree4 + name;
+  document.getElementById('greetings').innerText = gree4 + name + '.';
 }
